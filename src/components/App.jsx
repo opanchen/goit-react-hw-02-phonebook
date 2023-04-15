911fb6171bbeac11107b80ac62d01b70887f8006
opanchen <panchenkooleh@gmail.com>
2023-04-16 00:06:59 +0300

    task 2 is done (demo)

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,73 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { nanoid } from 'nanoid'
+import { Component } from "react";
+import { ContactForm, ContactList, Filter} from "components";
+import css from './App.module.css'
+
+export class App extends Component {
+
+  state = {
+    contacts: [
+      {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+      {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+      {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+      {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+    ],
+    filter: '',
+  }
+
+  formSubmitHandler = ({name, number}) => {
+    const id = nanoid();
+    const newContact = {
+      name,
+      number,
+      id,
+    }
+    this.setState(({contacts}) => ({contacts: [...contacts, newContact]}))
+  }
+
+  changeFilter = (e) => {
+    this.setState({filter: e.currentTarget.value})
+  }
+
+  getVisibleContacts = () => {
+    const {filter, contacts} = this.state;
+    const normalizedFilter = filter.toLowerCase();
+    return contacts.filter(({name}) => name.toLowerCase().includes(normalizedFilter));
+    
+  }
+
+  deleteContact = (idToDelete) => {
+    this.setState((prevState) => ({contacts: prevState.contacts.filter((item) => item.id !== idToDelete)}))
+  }
+
+  checkContactName = (query) => {
+    const {contacts} = this.state;
+    return contacts.some(({name}) => name.toLowerCase() === query.toLowerCase())
+
+  }
+
+  render() {
+    const visibleContacts = this.getVisibleContacts()
+
+    return (
+      <div className={css.app}>
+      <h1>Phonebook</h1>
+      <ContactForm 
+      onSubmit={this.formSubmitHandler}
+      checkContactName={this.checkContactName}
+      />
+
+      <h2>Contacts</h2>
+      <Filter
+        value={this.state.filter}
+        onChange={this.changeFilter}
+      />
+      <ContactList
+      contactList={visibleContacts}
+      handleDeleteContact={this.deleteContact}
+      />
+      </div>
+    )
+  }
+
+}
